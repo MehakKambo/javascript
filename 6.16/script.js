bcb0d1fcf70a4d65f279b3545aafdf23a6c6b855
MehakKambo <mehakpreetp12@gmail.com>
2023-01-19 23:09:36 -0800

    Finish if-else js

--- 6.16/script.js
+++ 6.16/script.js
@@ -1,6 +1,8 @@
-// Your solution goes here 
-
-
+/*
+Checks if password is at least 8 chars long,
+doesn't include "password" and
+has at least one upper case char.
+*/
 function isStrongPassword(password) {
     if(password.length >=8 && 
         !(password.includes("password")) &&
