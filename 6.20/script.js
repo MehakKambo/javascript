4ccca51d58a32021541064ddb56e608ef9d8b7f4
MehakKambo <mehakpreetp12@gmail.com>
2023-01-24 23:02:19 -0800

    Finish JS Map DS

--- 6.20/script.js
+++ 6.20/script.js
@@ -1,3 +1,17 @@
 function calcWordFrequencies() {
-   
+   let input = prompt("Enter words: ");
+   let words = input.split(" ");
+   let wordFrequency = new Map();
+
+   for(let i = 0; i < words.length; i++) {
+    if(wordFrequency.has(words[i])) {
+        wordFrequency.set(words[i], wordFrequency.get(words[i]) + 1);
+    } else {
+        wordFrequency.set(words[i], 1);
+    }
+   }
+
+   for (let [word, frequency] of wordFrequency) {
+    console.log(word + " " + frequency);
+   }
 }
