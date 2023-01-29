8d6a9d4daf90a504f9f6f89b64ebdd6b5023fe53
MehakKambo <mehakpreetp12@gmail.com>
2023-01-29 11:59:04 -0800

    Conversions done

--- CH7/7.15/convert.js
+++ CH7/7.15/convert.js
@@ -7,10 +7,16 @@ function domLoaded() {
 
 function convertCtoF(degreesCelsius) {
    // TODO: Complete the function
+   let tempInC = parseFloat(degreesCelsius);
+   let tempInF = tempInC * 9/5 + 32;
 
+   return tempInF;
 }
 
 function convertFtoC(degreesFahrenheit) {
    // TODO: Complete the function
-   
+   let tempInF = parseFloat(degreesFahrenheit); 
+   let tempInC = (tempInF - 32) * 5/9;
+
+   return tempInC;
 }
