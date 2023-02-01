1a69dad43c10ada3e2c920b3c830e4a462954ff2
MehakKambo <mehakpreetp12@gmail.com>
2023-01-31 18:10:46 -0800

    Finished Conversion Lab

--- CH7/7.15/convert.js
+++ CH7/7.15/convert.js
@@ -2,7 +2,59 @@ window.addEventListener("DOMContentLoaded", domLoaded);
 
 function domLoaded() {
    // TODO: Complete the function
+   document.getElementById("convertButton").addEventListener("click", function() {
+      let cel = document.getElementById("cInput").value;
+      let fah = document.getElementById("fInput").value;
+      let celsius = parseFloat(document.getElementById("cInput").value);
+      let fahrenheit = parseFloat(document.getElementById("fInput").value);
+      let errorMsg = document.getElementById("errorMessage");
+      let imgElement = document.getElementById("weatherImage");
+      
+      //set empty by default
+      errorMsg.innerHTML = "";
+      
+      //if cel is a num 
+      if(!isNaN(celsius)) {
+         fahrenheit = convertCtoF(celsius);
+         document.getElementById("fInput").value = fahrenheit;
+      } 
+      
+      //if fahr is a num
+      else if(!isNaN(fahrenheit)) {
+         celsius = convertFtoC(fahrenheit);
+         document.getElementById("cInput").value = celsius;
+      } 
+      
+      //if cel is str and fah is empty
+      else if(isNaN(celsius) && fah === ""){
+         errorMsg.innerHTML = cel + " is not a number"; 
+      } 
+      
+      //if fah is str and cel is empty
+      else if(isNaN(fahrenheit) && cel === "") {
+         errorMsg.innerHTML = fah + " is not a number"; 
+      } 
+      
+      
+      if(fahrenheit < 32) {
+         imgElement.src = "cold.png";
+      } else if(fahrenheit >= 32 && fahrenheit <= 50) {
+         imgElement.src = "cool.png";
+      } else if(fahrenheit > 50) {
+         imgElement.src = "warm.png";
+      }
 
+   });
+
+   document.getElementById("fInput").addEventListener("input", clearCel);
+   function clearCel() {
+      document.getElementById("cInput").value = "";
+   }
+   
+   document.getElementById("cInput").addEventListener("input", clearFah);
+   function clearFah() {
+      document.getElementById("fInput").value = "";
+   }
 }
 
 function convertCtoF(degreesCelsius) {
